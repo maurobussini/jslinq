b9e2e055fbf6fc89418ecd47fec0f9903d6b8b66
Mauro Bussini <zenprogramming.it@gmail.com>
2016-09-07 21:32:45 +0200

    version 1.0.18

--- src/jslinq.d.ts
+++ src/jslinq.d.ts
@@ -1,80 +1,163 @@
-declare function jslinq(data: any[]) : jslinq.JsLinq; 
+declare function jslinq(data: any[]) : jslinq.JsLinq;
+declare function jslinq<T>(data: T[]) : jslinq.JsLinq<T>; 
 
 declare namespace jslinq {
 
-    interface JsLinq {
+    // interface JsLinq {
+        
+    //     //Where
+    //     where?: (condition: (el: any) => boolean) => JsLinq;
+
+    //     //Select
+    //     select: (projection: (el) => any) => JsLinq;
+
+    //     //GroupBy
+    //     groupBy?: (group: (el) => any) => JsLinq;
+
+    //     //Join
+    //     join?: (otherData: any[]) => JsLinq;
+
+    //     //ToList
+    //     toList?: () => any[];
+
+    //     //Intersect
+    //     intersect?: (otherData: any[], comparisonField?: (el) => any) => JsLinq;
+
+    //     //Distinct
+    //     distinct?: () => JsLinq;
+
+    //     //OrderBy
+    //     orderBy?: (sortField: (el) => any) => JsLinq;
+
+    //     //OrderByDescending
+    //     orderByDescending?: (sortField: (el) => any) => JsLinq;
+
+    //     //SelectMany
+    //     selectMany?: (projection: (el) => any[]) => JsLinq;
+
+    //     //SingleOrDefault
+    //     singleOrDefault?: (condition?: (el: any) => boolean) => any;
+
+    //     //FirstOrDefault
+    //     firstOrDefault?: (condition?: (el: any) => boolean) => any;
+
+    //     //LastOrDefault
+    //     lastOrDefault?: (condition?: (el: any) => boolean) => any;
+
+    //     //Any
+    //     any?: (condition?: (el: any) => boolean) => boolean;
+
+    //     //All
+    //     all?: (condition?: (el: any) => boolean) => boolean;
+
+    //     //Count
+    //     count?: () => number;
+
+    //     //Skip
+    //     skip?: (quantity: number) => JsLinq;
+
+    //     //Take
+    //     take?: (quantity: number) => JsLinq;
+
+    //     //Max
+    //     max?: (projection?: (el) => any) => any;
+
+    //     //Min
+    //     min?: (projection?: (el) => any) => any;
+
+    //     //Remove
+    //     remove?: (element: any) => JsLinq;
+
+    //     //Subtract
+    //     subtract?: (otherData: any[], comparisonField?: (el) => any) => JsLinq;
+
+    //     //Sum
+    //     sum?: (projection?: (el) => number) => number;
+
+    //     //Average
+    //     average?: (projection?: (el) => number) => number;            
+    // }
+
+    interface JsLinq<T> {
         
         //Where
-        where?: (condition: (el: any) => boolean) => JsLinq;
+        where?: (condition: (el: T) => boolean) => JsLinq<T>;
 
         //Select
-        select: (projection: (el) => any) => JsLinq;
+        select: (projection: (el) => T) => JsLinq<T>;
 
         //GroupBy
-        groupBy?: (group: (el) => any) => JsLinq;
+        groupBy?: (group: (el) => T) => JsLinqGroup<T>;
 
         //Join
-        join?: (otherData: any[]) => JsLinq;
+        join?: (otherData: T[]) => JsLinq<T>;
 
         //ToList
-        toList?: () => any[];
+        toList?: () => T[];
 
         //Intersect
-        intersect?: (otherData: any[], comparisonField?: (el) => any) => JsLinq;
+        intersect?: (otherData: T[], comparisonField?: (el) => T) => JsLinq<T>;
 
         //Distinct
-        distinct?: () => JsLinq;
+        distinct?: () => JsLinq<T>;
 
         //OrderBy
-        orderBy?: (sortField: (el) => any) => JsLinq;
+        orderBy?: (sortField: (el) => T) => JsLinq<T>;
 
         //OrderByDescending
-        orderByDescending?: (sortField: (el) => any) => JsLinq;
+        orderByDescending?: (sortField: (el) => T) => JsLinq<T>;
 
         //SelectMany
-        selectMany?: (projection: (el) => any[]) => JsLinq;
+        selectMany?: (projection: (el) => T[]) => JsLinq<T>;
 
         //SingleOrDefault
-        singleOrDefault?: (condition?: (el: any) => boolean) => JsLinq;
+        singleOrDefault?: (condition?: (el: T) => boolean) => T;
 
         //FirstOrDefault
-        firstOrDefault?: (condition?: (el: any) => boolean) => JsLinq;
+        firstOrDefault?: (condition?: (el: T) => boolean) => T;
 
         //LastOrDefault
-        lastOrDefault?: (condition?: (el: any) => boolean) => JsLinq;
+        lastOrDefault?: (condition?: (el: T) => boolean) => T;
 
         //Any
-        any?: (condition?: (el: any) => boolean) => boolean;
+        any?: (condition?: (el: T) => boolean) => boolean;
 
         //All
-        all?: (condition?: (el: any) => boolean) => boolean;
+        all?: (condition?: (el: T) => boolean) => boolean;
 
         //Count
         count?: () => number;
 
         //Skip
-        skip?: (quantity: number) => JsLinq;
+        skip?: (quantity: number) => JsLinq<T>;
 
         //Take
-        take?: (quantity: number) => JsLinq;
+        take?: (quantity: number) => JsLinq<T>;
 
         //Max
-        max?: (projection?: (el) => any) => any;
+        max?: (projection?: (el) => T) => T;
 
         //Min
-        min?: (projection?: (el) => any) => any;
+        min?: (projection?: (el) => T) => T;
 
         //Remove
-        remove?: (element: any) => JsLinq;
+        remove?: (element: T) => JsLinq<T>;
 
         //Subtract
-        subtract?: (otherData: any[], comparisonField?: (el) => any) => JsLinq;
+        subtract?: (otherData: T[], comparisonField?: (el) => T) => JsLinq<T>;
 
         //Sum
         sum?: (projection?: (el) => number) => number;
 
         //Average
         average?: (projection?: (el) => number) => number;            
+    }
+
+    interface JsLinqGroup<T> {
+
+        key: any;
+        count: number; 
+        elements: T[];
     }
 }
 
